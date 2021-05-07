e1c38a271aecb9a6456ea761a9827aeeff42a467
91986 <91986@LAPTOP-KB18ES8T>
2021-05-07 19:56:49 +0530

    update

---
+++ BuildProject.UITest1/AppInitializer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+namespace BuildProject.UITest1
+{
+    public class AppInitializer
+    {
+        public static IApp StartApp(Platform platform)
+        {
+            if (platform == Platform.Android)
+            {
+                return ConfigureApp.Android.InstalledApp("com.companyname.buildproject").StartApp();
+            }
+
+            return ConfigureApp.iOS.StartApp();
+        }
+    }
+}
